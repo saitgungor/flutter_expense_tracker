51bfcec8e387cc6981933e2298ab53788e4c6ca0
Osman Sait Güngör <sait@bronixengineering.com>
2023-08-15 16:06:53 +0300

    Expense list implemented with adummy list

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:fluttter_expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:fluttter_expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter',
+      amount: 125,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Dinner',
+      amount: 24.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Flutter',
+      amount: 16,
+      date: DateTime.now(),
+      category: Category.food,
+    )
+  ];
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Cart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
